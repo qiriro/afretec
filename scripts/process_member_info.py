6d95749781b887f797cf96e8c13016b8408cbf1b
qiriro <nkuzito@gmail.com>
2023-12-08 17:02:23 +0000

    Deploying to gh-pages from @ qiriro/afretec@ae895d45b9b223dc11c1114a6f2ae8a4622b54be 🚀

--- scripts/process_member_info.py
+++ scripts/process_member_info.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import re
 import gdown
-from PIL import Image
+from PIL import Image, ImageFilter
 def download_member_csv():
     print("Downloading member information from Google Sheets")
     file_url ="https://docs.google.com/spreadsheets/d/1pecx-Dt7CR7h5_yngS_Wwr-i5ONi_HOeiaVnb_A1L5E/edit?usp=sharing"
@@ -10,16 +10,21 @@ def download_member_csv():
     sheet_name = 'member-information'
     url=f'https://docs.google.com/spreadsheet/ccc?key={gsheetkey}&output=xlsx'
     df = pd.read_excel(url,sheet_name=sheet_name)
-    current_dir = os.path.dirname(__file__)
-    raw_csv_file = os.path.join(current_dir, '../raw_data/afretec-member-information.csv')
+   
+    current_dir = os.getcwd()
+    output_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+    output_dir = os.path.join(current_dir, 'raw_data')
+    os.makedirs(output_dir, exist_ok=True)
+    raw_csv_file = os.path.join(output_dir, 'afretec-member-information.csv')
     print(f"Saving raw member information to {raw_csv_file}")
     df.to_csv(raw_csv_file, index=False)
 def add_image_file_name(df):
-    df['PhotoUrl'] = df.apply(lambda row: get_image_file_name(row), axis=1)
+    df['Image'] = df.apply(lambda row: get_image_file_name(row), axis=1)
+    del df['Photo']
     return df
 def download_images(df):
     root_path = os.path.join(os.path.dirname(__file__),
-                                 '../assets/img/members')
+                                 '../assets/img/members/raw')
     os.makedirs(root_path, exist_ok=True)
     
     for _, row in df.iterrows():
@@ -36,12 +41,11 @@ def get_image_file_name(row):
     file_name = None 
     if len(str(row['Photo'])) < 10:
         if row['Gender'] == "Male":
-            file_name = "/assets/img/members/male-person.jpg"
+            file_name = "male-person.jpg"
         else:
-            file_name = "/assets/img/members/female-person.jpg"
+            file_name = "female-person.jpg"
     else: 
-        name =f"{row['FirstName']}_{row['LastName']}.jpg".replace(" ", "")
-        file_name = f"/assets/img/members/{name}"
+        file_name =f"{row['FirstName']}_{row['LastName']}.jpg".replace(" ", "")
     return file_name
     
 def construct_image_url(url):
@@ -79,6 +83,7 @@ def clean_member_info():
     df = pd.read_csv(raw_csv_file)
     df = rename_columns(df)
     df=add_image_file_name(df)
+    df['Website'] = df['Website'].apply(clean_member_website)
     renamed_csv_file = os.path.join(current_dir,
                                     '../_data/members.csv')
     print(f"Saving renamed DataFrame to {renamed_csv_file}")
@@ -88,17 +93,54 @@ def clean_member_info():
     #print("Deleting the raw CSV file")
     #os.remove(raw_csv_file)
     return df 
+def clean_member_website(row):
+    #check if the row is a websie or not
+    if not str(row).startswith("http"):
+        return ""
+    else:
+        return row
 def extract_drive_id(url):
     if len(str(url)) <10:
         return None
     pattern = r'(?<=id=)[a-zA-Z0-9_-]+'
     match = re.search(pattern, url)
     return match.group() if match else None
+def resize_and_compress_images(quality=85):
+    # Create the output directory if it doesn't exist
+    input_dir =os.path.join(os.path.dirname(__file__),
+                                 '../assets/img/members/raw')
+    output_dir =os.path.join(os.path.dirname(__file__),
+                                 '../assets/img/members/resized')
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Loop through each file in the input directory
+    for filename in os.listdir(input_dir):
+        if filename.endswith((".jpg", ".jpeg", ".png")):
+            # Construct the full file paths
+            input_path = os.path.join(input_dir, filename)
+            output_path = os.path.join(output_dir, filename)
+
+            # Open the image
+            with Image.open(input_path) as img:
+                if img.mode == 'RGBA':
+                    img = img.convert('RGB')
+                mywidth = 600
+                wpercent = (mywidth/float(img.size[0]))
+                hsize = int((float(img.size[1])*float(wpercent)))
+                
+                # Resize the image to the target size
+                resized_img = img.resize((mywidth,hsize), resample=Image.LANCZOS)
+
+                # Save the resized and compressed image
+                resized_img.save(output_path, optimize=True, quality=quality)
+
+                
 
 if __name__ == '__main__':
-    download_member_csv()
+    #download_member_csv()
     df = clean_member_info()
-    download_images(df)
+    #download_images(df)
+    #resize_and_compress_images()
     
    
    
